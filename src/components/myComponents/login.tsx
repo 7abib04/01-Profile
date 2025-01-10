b8c8c1cf5f3520c32cd560d82147f655d601ae5f
hmansoor <7abib2004@gmail.com>
2025-01-10 23:32:45 +0300

    login design without handling

---
+++ src/components/myComponents/login.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+// import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { AtSign, Lock, ArrowRight } from 'lucide-react'
+
+export default function LoginForm() {
+
+
+
+
+
+
+    // TODO the handling for login (from prev project)
+
+  return (
+    <form  className="space-y-6">
+      <div className="space-y-6">
+        <div className="space-y-2">
+          <Label htmlFor="email" className="text-sm font-medium text-gray-300">Email</Label>
+          <div className="relative">
+            <Input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              required
+              className="pl-10 w-full py-3 bg-gray-800 text-white placeholder-gray-500 border-gray-700 rounded-lg focus:ring-2 focus:ring-[rgb(153,105,255)] focus:border-transparent transition-all duration-300"
+            />
+            <AtSign className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" size={18} />
+          </div>
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="password" className="text-sm font-medium text-gray-300">Password</Label>
+          <div className="relative">
+            <Input
+            
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+            
+              required
+              className="pl-10 w-full py-3 bg-gray-800 text-white placeholder-gray-500 border-gray-700 rounded-lg focus:ring-2 focus:ring-[rgb(153,105,255)] focus:border-transparent transition-all duration-300"
+            />
+            <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" size={18} />
+          </div>
+        </div>
+      </div>
+     
+
+
+
+      <Button 
+        type="submit" 
+        className="w-full flex justify-center items-center py-4 px-4 border border-transparent rounded-lg shadow-sm text-lg font-medium text-white bg-[rgb(153,105,255)] hover:bg-[rgb(173,135,255)] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[rgb(153,105,255)] transition-all duration-300"
+      >
+        Sign in
+        <ArrowRight className="ml-2" size={20} />
+      </Button>
+      
+    </form>
+  )
+}
+
